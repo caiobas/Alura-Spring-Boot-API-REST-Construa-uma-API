e4b418bccefa3c9de33c75fb843fc15cbe4f5e8f
Caiobas <caiobas6@hotmail.com>
2020-11-30 12:42:00 -0300

    04.Trabalhando com POST - Cadastrando tópicos

---
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,40 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class TopicoForm {
+    private String titulo;
+    private String mensagem;
+    private String nomeCurso;
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public Topico converter(CursoRepository cursoReposity){
+        Curso curso = cursoReposity.findByNome(nomeCurso);
+        return new Topico(titulo, mensagem, curso);
+    }
+}
